a9aa5e2fb23fa878572367ebb1ccafed2ad9d24a
= <jacek@galowicz.de>
2024-10-09 10:47:58 +0200

    drop code by using unique_ptr

--- include/mystring.hpp
+++ include/mystring.hpp
@@ -1,20 +1,20 @@
 #include <cstddef>
+#include <memory>
 
 class MyString {
     size_t length_ {0};
-    char *content_ {nullptr};
+    std::unique_ptr<char[]> content_;
 
 public:
     MyString() = default;
     MyString(const char *);
     MyString(const char *, size_t );
-    ~MyString();
 
     MyString(const MyString &);
     MyString& operator=(const MyString &);
 
-    MyString(MyString &&);
-    MyString& operator=(MyString &&);
+    MyString(MyString &&) = default;
+    MyString& operator=(MyString &&) = default;
 
     MyString operator+(const MyString &) const;
     bool operator==(const MyString &) const;
--- src/mystring.cpp
+++ src/mystring.cpp
@@ -3,9 +3,9 @@
 #include <cstring>
 #include <iostream>
 
-static char* allocate_and_fill(const char *source, size_t length) {
-  auto content {new char[length+1]};
-  std::memcpy(content, source, length+1);
+static std::unique_ptr<char[]> allocate_and_fill(const char *source, size_t length) {
+  auto content {std::make_unique_for_overwrite<char[]>(length+1)};
+  std::memcpy(content.get(), source, length+1);
   return content;
 }
 
@@ -17,61 +17,34 @@ MyString::MyString(const char *s, size_t len)
   : length_{len}
   , content_{allocate_and_fill(s, len)}
   {
-    std::cout << "CTOR with " << (content_ ? content_ : "empty") << '\n';
+    std::cout << "CTOR with " << (content_ ? content_.get() : "empty") << '\n';
   }
 
 MyString::MyString(const MyString &other) 
-  : MyString{other.content_, other.length_}
+  : MyString{other.content_.get(), other.length_}
   {
-    std::cout << "COPY CTOR with " << (content_ ? content_ : "empty") << '\n';
+    std::cout << "COPY CTOR with " << (content_ ? content_.get() : "empty") << '\n';
   }
-
-MyString::MyString(MyString &&other) 
-  : length_{other.length_}
-  , content_{other.content_}
-{
-  std::cout << "MOVE CTOR with " << (content_ ? content_ : "empty") << '\n';
-  other.content_ = nullptr;
-}
 
 
 MyString& MyString::operator=(const MyString &other) {
-    delete[] content_;
-    content_ = allocate_and_fill(other.content_, other.length_);
+    content_ = allocate_and_fill(other.content_.get(), other.length_);
     length_ = other.length_;
 
-    std::cout << "OPERATOR= with " << (content_ ? content_ : "empty") << '\n';
+    std::cout << "OPERATOR= with " << (content_ ? content_.get() : "empty") << '\n';
     return *this;
-}
-
-MyString& MyString::operator=(MyString &&other) {
-    std::swap(content_, other.content_);
-    std::swap(length_, other.length_);
-    std::cout << "MOVE OPERATOR= with " << (content_ ? content_ : "empty") << '\n';
-    return *this;
-}
-
-
-
-MyString::~MyString() {
-    if (content_ != nullptr) {
-      std::cout << "Destroying \"" << content_ << "\"\n";
-    } else {
-      std::cout << "Destroying empty string\n";
-    }
-    delete[] content_;
 }
 
 size_t MyString::length() const { return length_; }
-const char * MyString::c_str() const { return content_; }
+const char * MyString::c_str() const { return content_.get(); }
 
 MyString MyString::operator+(const MyString &other) const {
     size_t len = length_ + other.length_;
-    char *dest = new char[len+1];
-    memcpy(dest, content_, length_);
-    memcpy(dest + length_, other.content_, other.length_ + 1);
+    auto dest = std::make_unique_for_overwrite<char[]>(len+1);
+    memcpy(dest.get(), content_.get(), length_);
+    memcpy(dest.get() + length_, other.content_.get(), other.length_ + 1);
     MyString s{}; 
     s.length_ = len;
-    s.content_ = dest;
+    s.content_= std::move(dest);
     return s;
 }
