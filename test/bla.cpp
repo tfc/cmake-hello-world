d406bd5808fef59a3a32941b02368af4f87f217c
= <jacek@galowicz.de>
2024-10-10 13:29:00 +0200

    more example test/syntax stuff

---
+++ test/bla.cpp
@@ -0,0 +1,59 @@
+#include <optional>
+#include <set>
+#include <unordered_set>
+#include <map>
+
+#include <gtest/gtest.h>
+
+std::optional<int> dividiere(int a, int b) {
+    if (b == 0) { return {}; }
+    return a / b;
+}
+
+TEST(dividiere, Optional) {
+    auto r = dividiere(10, 2);
+    EXPECT_TRUE(r);
+    EXPECT_EQ(*r, 5);
+    EXPECT_FALSE(dividiere(10, 0));
+}
+
+std::optional<std::tuple<int, int>> dividiere_tup(int a, int b) {
+    if (b == 0) { return {}; }
+    return {{a / b, a % b}};
+}
+
+TEST(dividiere_tup, Optional) {
+    auto r = dividiere_tup(10, 3);
+    EXPECT_TRUE(r);
+    const auto& [quotient, modulo] = *r;
+    EXPECT_EQ(quotient, 3);
+    EXPECT_EQ(modulo, 1);
+}
+
+TEST(set, set) {
+    std::unordered_set<int> s{3, 2, 1, 3};
+
+    if (const auto& [it, success] {s.insert(10)}; success) {
+        std::cout << *it << " wurde hinzugefuegt!\n";
+    }
+
+    for (const auto& i : s) {
+        std::cout << i << ", ";
+    }
+    std::cout << '\n';
+}
+
+TEST(map, map) {
+    std::map<size_t, size_t> fibos {
+        {1, 1},
+        {2, 1},
+        {3, 2}
+    };
+
+
+
+    for (const auto &[key, value] : fibos) {
+        std::cout << "fibo(" << key << ") = " << value << '\n';
+    }
+}
+
--- test/fibo.cpp
+++ test/fibo.cpp
@@ -13,7 +13,9 @@ TEST(IteratorComputesCorrectly, FibonacciIterator) {
 
 TEST(IteratorIterates, FibonacciIterator) {
   fibo_range r {4};
-  std::vector<size_t> v;
+  using number_type = decltype(*r.begin());
+  std::vector<number_type> v;
+  using vector_type = decltype(v);
   std::copy(r.begin(), r.end(), std::back_inserter(v));
-  EXPECT_EQ(v, (std::vector<size_t>{1, 1, 2, 3}));
+  EXPECT_EQ(v, (vector_type{1, 1, 2, 3}));
 }
