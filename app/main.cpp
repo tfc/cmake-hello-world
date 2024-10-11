4e5c3b836e3ff9b7c06f9a6ef4be107ac696bbd7
= <jacek@galowicz.de>
2024-10-11 09:59:01 +0200

    import/export matrix gmbh

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <matrix.hpp>
+#include <fstream>
 
 int main()
 {
-    Matrix<double, 3, 3> m {1,2,3,4,5,6,7,8,9};
-    std::cout << m << '\n';
+    std::ifstream f {"matrix.txt"};
+    if (auto om = Matrix<double, 3, 3>::import(f);
+        om) {
+        std::cout << *om << '\n';
+        std::ofstream dumpfile {"dump.txt"};
+        if (om->exporrt(dumpfile)) {
+            std::cout << "yes, export hat geklappt\n";
+        }
+    } else {
+        std::cout << "das hat nicht geklappt.\n";
+    }
+
 }
--- include/matrix.hpp
+++ include/matrix.hpp
@@ -2,33 +2,69 @@
 #include <array>
 #include <iostream>
 #include <initializer_list>
+#include <fstream>
+#include <optional>
+#include <iterator>
 
 template <typename T, size_t ROWS, size_t COLS>
-class Matrix {
+class Matrix
+{
     std::array<T, ROWS * COLS> v;
 
 public:
     Matrix() = default;
-    Matrix(std::initializer_list<T> il) 
+    Matrix(std::initializer_list<T> il)
     {
         std::copy(il.begin(), il.end(), v.begin());
     }
 
-    const T& operator()(size_t row, size_t col) const {
+    const T &operator()(size_t row, size_t col) const
+    {
         return v[row * COLS + col];
     }
-    
-    T& operator()(size_t row, size_t col) {
-        return const_cast<T&>(const_cast<const Matrix&>(*this)(row, col));
+
+    T &operator()(size_t row, size_t col)
+    {
+        return const_cast<T &>(const_cast<const Matrix &>(*this)(row, col));
+    }
+
+    static std::optional<Matrix> import(std::ifstream &input)
+    {
+        Matrix m;
+
+        for (size_t row{0}; row < ROWS; ++row)
+        {
+            for (size_t col{0}; col < COLS; ++col)
+            {
+                T current;
+                if (!(input >> current))
+                {
+                    return {};
+                }
+                m(row, col) = current;
+            }
+        }
+
+        std::string x;
+        if (input >> x) { return {}; }
+        return m;
+    }
+
+    bool exporrt(std::ostream &output) {
+        std::copy(v.begin(), v.end(), std::ostream_iterator<T>(output, " "));
+        return bool(output);
     }
 };
 
 template <typename T, size_t Rows, size_t Cols>
-std::ostream& operator<<(std::ostream& os, const Matrix<T, Rows, Cols> &m) {
+std::ostream &operator<<(std::ostream &os, const Matrix<T, Rows, Cols> &m)
+{
     os << "{\n";
-    for (size_t row {0}; row < Rows; ++row) {
+    for (size_t row{0}; row < Rows; ++row)
+    {
         os << " { ";
-        for (size_t col {0}; col < Cols; ++col) {
+        for (size_t col{0}; col < Cols; ++col)
+        {
             os << m(row, col) << ", ";
         }
         os << "}\n";
